168f9bb88d552728ac5ed6ff5c9471d863d6f326
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-11 13:23:36 +0900

    Add switch statements for operate function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case "add":
+            return add(x, y);
+        case "subtract":
+            return subtract(x, y);
+        case "multiply":
+            return multiply(x, y);
+        case "divide":
+            return divide(x, y);
+    }
+}
+
+console.log(operate("add", 2, 3));
