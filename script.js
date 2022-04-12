d9c8bde4caa2a43eac9daecf48da0a5cf9dd88de
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-12 22:49:12 +0900

    Add comments on how to display numbers using an algorithm

--- script.js
+++ script.js
@@ -31,10 +31,31 @@ console.log(operate("add", 2, 3));
 
 const display = document.querySelector(".display");
 const displayText = document.querySelector(".displaytext");
+const toDisplay = [...document.querySelectorAll(".todisplay")];
+
+
+
+// a function that changes the display to show the number being clicked
+// function displayNumber(number) {
+//     displayText.innerText = "";
+//     const div = document.createElement("div");
+//     div.innerText = `${this}`;
+//     display.appendChild(div);
+// }
+
+// function addNumberEvent(item) {
+//     this.addEventListener("click", displayNumber());
+// }
+
+// toDisplay.forEach(addNumberEvent);
+
+// must set up a forEach function that adds the onclick eventListener to each number button
+// the click event should call a function that sets the display's innerText to the array item's
+// innerText because each array item in toDisplay (the array) has its number in the innerText property
 
 const zero = document.querySelector(".zero");
 zero.addEventListener("click", () => {
-    displayText.innerText="";
+    displayText.innerText = "";
     const div = document.createElement("div");
     div.innerText = "0"
     display.appendChild(div);
