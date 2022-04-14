0c0e6b734ab8483a9cf5c6640557fcc2725c7acc
Jeffrey Li <lijeffre@grinnell.edu>
2022-04-14 10:46:22 +0900

    Add DOM grabs of operators

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ function displayNumber() {
 }
 displayNumber();
 
+// add event listeners to the operators
+const divideButton = document.querySelector(".divide");
+const multiplyButton = document.querySelector(".multiply");
+const addButton = document.querySelector(".add");
+const subtractButton = document.querySelector(".subtract");
+
 
 // must set up a forEach function that adds the onclick eventListener to each number button
 // the click event should call a function that sets the display's innerText to the array item's
